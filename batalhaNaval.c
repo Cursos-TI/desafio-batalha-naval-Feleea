98290856f903aa10976f2d17b17ab35484bf77fe
Felipe Oliveira <139580193+Feleea@users.noreply.github.com>
2025-06-06 02:15:35 +0000

    Nível novato completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // Inicialização do cabeçalho
+    char linha[11] = {'#', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Inicializando a matriz
+    int tabuleiro[10][10];
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0; // 
+        }
+    }
+
+    // Imprimindo o cabeçalho
+    printf(" ");
+    for (int x = 0; x < 11; x++){
+        printf("%c ", linha[x]);
+    }
+    printf("\n");
+    
+    // Acertando os navios
+    tabuleiro[1][3] = 3;
+    tabuleiro[1][4] = 3;
+    tabuleiro[1][5] = 3;
+    tabuleiro[7][2] = 3;
+    tabuleiro[8][2] = 3;
+    tabuleiro[9][2] = 3;
+
+    // Imprimindo o tabuleiro
+    for (int i = 0; i < 10; i++) {
+        if (i >= 9){
+            printf("%d ", i+1);
+        } else {
+            printf(" %d ", i+1);
+        }
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
